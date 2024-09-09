cfb5425293ffea6e08102d71906da09c09f5bcb9
John Mannix <1419878+johnmannix@users.noreply.github.com>
2024-09-09 21:36:08 +0100

    Refinement

--- Scripts/ScriptProcessors/ab-reference/Interface.js
+++ Scripts/ScriptProcessors/ab-reference/Interface.js
@@ -2,10 +2,14 @@ Content.makeFrontInterface(600, 600);
 
 Content.getComponent("Button1").setControlCallback(onButton1Control);
 Content.getComponent("Frequency").setControlCallback(onFrequencyControl);
+Content.getComponent("BandWidth").setControlCallback(onBandwidthControl);
 const var FloatingTile1 = Content.getComponent("FloatingTile1");
+const var FrequencyValue = Content.getComponent("FrequencyValue");
+const var WidthValue = Content.getComponent("WidthValue");
 
 const player = Synth.getEffect("AudioPlayer");
 const EQ = Synth.getEffect("EQ");
+var bandwidth = 2.5;
 
 inline function onButton1Control(component, value)
 {
@@ -19,12 +23,26 @@ inline function onButton1Control(component, value)
 
 inline function onFrequencyControl(component, value)
 {
+	local v = Engine.doubleToString(value, 0) + " Hz";
+	FrequencyValue.set("text", v);
+	
 	EQ.setAttribute(EQ.BandOffset * 0 + EQ.Freq, value);
 	EQ.setAttribute(EQ.BandOffset * 1 + EQ.Freq, value);
-	EQ.setAttribute(EQ.BandOffset * 2 + EQ.Freq, value * 2.5);
-	EQ.setAttribute(EQ.BandOffset * 3 + EQ.Freq, value * 2.5);	
+	EQ.setAttribute(EQ.BandOffset * 2 + EQ.Freq, value * bandwidth);
+	EQ.setAttribute(EQ.BandOffset * 3 + EQ.Freq, value * bandwidth);
 };
 
+inline function onBandwidthControl(component, value)
+{
+	bandwidth = value;
+	
+	local l = Engine.doubleToString(value, 1);
+	WidthValue.set("text", l);
+	
+	local v = EQ.getAttribute(EQ.BandOffset * 0 + EQ.Freq);
+	EQ.setAttribute(EQ.BandOffset * 2 + EQ.Freq, v * bandwidth);
+	EQ.setAttribute(EQ.BandOffset * 3 + EQ.Freq, v * bandwidth);
+};
 
 inline function onFilterControl(component, value)
 {
